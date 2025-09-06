d124571a8432d61b4e579565e409ef9c5d9bb8da
TamuraYui <yuitamura0929@gmail.com>
2025-09-06 10:24:28 +0900

    [add]TamuraFolder>ScriptsにSkillDataとSkillBaseを追加。

---
+++ Assets/_MembersFolder/Tamura/Scripts/SkillData.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スキルの共有パラメータ
+/// </summary>
+[CreateAssetMenu(menuName = "Scriptables/Create SkillData")]
+public class SkillData : ScriptableObject
+{
+    [Header("スキル名")]
+    public string skillName;
+    [Header("スキル効果説明")]
+    public string skillDescription;
+    [Header("正気度コスト")]
+    public int sanityCost;
+    [Header("命中率")]
+    public int accuracy;
+    [Header("攻撃倍率")]
+    public int atkMult;
+    [Header("防御倍率")]
+    public int defMult;
+    [Header("追加効果確率")]
+    public int effectProbability;
+    [Header("ステータス上昇倍率")]
+    public float statusBoost;
+    [Header("ステータス下降倍率")]
+    public float statusDecrease;
+}
