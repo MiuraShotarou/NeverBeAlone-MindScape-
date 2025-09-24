c84532cc36b4d31a97d0618d51a2e906b6d56b16
nzkiya5492 <nzkiya5492@gmail.com>
2025-09-24 20:31:57 +0900

    ConditionBase完成、自傷、ドレイン実装中

--- Assets/_C_Data/D_Common/Conditions/ConditionAnger/ConditionDrain.cs
+++ Assets/_C_Data/D_Common/Conditions/ConditionAnger/ConditionDrain.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConditionDrain : MonoBehaviour
+/// <summary>与えたダメージ×0.1回復</summary>
+public class ConditionDrain : ConditionBase
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Emotion _emotion = Emotion.Anger;
+    private float _hpRestoreRate = 0.1f;
+    private int _targetHp = default;
+
+    /// <summary>状態異常のクラスとビットフラグを紐づける</summary>
+    public ConditionDrain(Condition condition) : base(condition)
     {
-        
+        //基底クラスで定義済み。何も書かなくてOK
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ApplyCondition()
     {
-        
+        _targetHp = Target.Hp;
+    }
+
+    public override void ReapplyCondition()
+    {
+        ActiveTurns = Random.Range(1, 3);
+    }
+
+    public override void ActivateConditionEffect()
+    {
+        //与えたダメージ
+        int damage = 0;
+        _targetHp += (int)(damage * _hpRestoreRate);
+    }
+
+    public override void RemoveCondition()
+    {
+        ActiveTurns = 0;
     }
 }
--- Assets/_C_Data/D_Common/Conditions/ConditionBase.cs
+++ Assets/_C_Data/D_Common/Conditions/ConditionBase.cs
@@ -1,32 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-/// <summary>
-/// SkillBaseが継承
-/// </summary>
-public abstract class ConditionBase
+public abstract class ConditionBase : MonoBehaviour
 {
-    protected Condition _condition;
-    public Condition ConditionName
+    private Condition _condition;
+    protected Condition ConditionName
     {
         get { return _condition; }
         set { _condition = value; }
     }
 
-    /// <summary>状態異常の効果</summary>
+    private int _activeTurns = 1;
+    /// <summary>継続ターン数</summary>
+    protected int ActiveTurns
+    {
+        get { return _activeTurns; }
+        set { _activeTurns = Mathf.Clamp(value, 1, 3); }
+    }
+
+    protected BattleUnitBase Target = default;
+
+    /// <summary>状態異常のクラスとビットフラグを紐づける</summary>
+    public ConditionBase(Condition condition)
+    {
+        _condition = condition;
+    }
+
+    /// <summary>状態異常を付与。必要なパラメーター取得等</summary>
     public abstract void ApplyCondition();
 
     /// <summary>同じ状態異常を付与されたとき</summary>
     public abstract void ReapplyCondition();
 
-    /// <summary>状態異常の解除</summary>
+    /// <summary>状態異常の効果が発動する瞬間の処理</summary>
+    public abstract void ActivateConditionEffect();
+
+    /// <summary>状態異常を解除</summary>
     public abstract void RemoveCondition();
 
-    /// <summary>ターゲットのビットフラグに追加</summary>
+    /// <summary>ターゲットに状態異常を付与してビットフラグに追加</summary>
     /// <param name="targetCondition"></param>
-    public void ApplyConditionToTarget(Condition targetCondition)
+    public void ApplyConditionToTarget(BattleUnitBase target)
     {
+        Target = target;
+        Condition targetCondition = target.ConditionFlag;
+
         if ((targetCondition & _condition) == _condition)
         {
             ReapplyCondition();
@@ -39,10 +59,11 @@ public abstract class ConditionBase
         Debug.Log(_condition + "状態になりました");
     }
 
-    /// <summary>ターゲットのビットフラグから削除</summary>
-    
-    public void RemoveConditionFromTarget(Condition targetCondition)
+    /// <summary>ターゲットの状態を戻してビットフラグから削除</summary>
+
+    public void RemoveConditionFromTarget(BattleUnitBase target)
     {
+        Condition targetCondition = target.ConditionFlag;
         RemoveCondition();
         targetCondition &= ~_condition;
         Debug.Log(_condition + "状態が解除されました");
